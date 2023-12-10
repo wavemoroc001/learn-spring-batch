deb7d7616bfb7a90fd9e6c60a36ef3b4e013e5da
Patipan Mankhong <patipan.m@arise.tech>
2023-12-10 10:46:36 +0700

    add kafka batch

---
+++ batch-insert-customer-from-csv/src/main/java/BatchKafkaApplication.java
@@ -0,0 +1,13 @@
+package org.example.batchkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchKafkaApplication.class, args);
+    }
+
+}
---
+++ batch-kafka/src/test/java/org/example/batchkafka/BatchKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.batchkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
